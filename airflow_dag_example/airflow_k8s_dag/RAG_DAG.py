9288b03d8d438643f86ff4731e50cfa8ea68a7e7
Tanguy Pomas <56535366+TPomas@users.noreply.github.com>
2025-01-24 17:06:30 +0100

    Adding simple airflow dag working on k8s

---
+++ airflow_dag_example/airflow_k8s_dag/RAG_DAG.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
+with DAG(
+  dag_id="RAG_DAG",
+  description="Description of RAG DAG",
+  schedule_interval=None) as dag:
+
+  t1 = KubernetesJobOperator(
+    name="parse-docs",
+    task_id="parse-docs",
+    job_template_file="<PATH_TO>/parsing-job.yaml" # /opt/airflow/dags if K8s job files are next to DAG files
+  )
+
+  t1 = KubernetesPodOperator(
+    name="embed-chunks",
+    task_id="embed-chunks",
+    job_template_file="<PATH_TO>/embedding-job.yaml" # /opt/airflow/dags if K8s job files are next to DAG files
+  )
+ 
+  t1 >> t2
